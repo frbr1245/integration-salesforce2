6afa60fe7a6bfd812537420aa26dc1f35e40097a
derek white <derekbwhite80@gmail.com>
2018-05-09 14:44:40 -0400

    edit tostring tests [#157436826]

--- src/Integration.Salesforce.Testing/Library/LibraryToStringTests.cs
+++ src/Integration.Salesforce.Testing/Library/LibraryToStringTests.cs
@@ -6,20 +6,18 @@ namespace Integration.Salesforce.Testing.Library.ModelTests
 {
     public class PersonToStringTests
     {
-        // ModelData md = new ModelData();
+        ModelData md = new ModelData();
 
         [Fact]
         public void ContactToString_ReturnsValidString_True()
         {
             // assemble
-            Person trainer = md.Trainer();
-            Address location = md.Location();
-            Batch batch = md.Batch();
+            Person person = md.Person();
+            Address location = md.Address();
             // act
-            var response = trainer.ToString();
-            var teststring = $"PERSON{{Name:{trainer.FirstName} {trainer.LastName};Phone:{trainer.Phone};Role:{trainer.Role};HasCar:{trainer.HasCar};BatchName:{trainer.BatchName};}}";
+            var response = person.ToString();
+            var teststring = $"PERSON{{Name:{person.FirstName} {person.LastName};Phone:{person.Phone};EMail:{person.EMail};Role:{person.Role};HasCar:{person.HasCar};BatchName:{person.BatchName};}}";
             teststring += location.ToString();
-            teststring += email.ToString();
             // assert
             Assert.NotNull(response);
             Assert.Equal(teststring, response, true);
@@ -29,7 +27,7 @@ namespace Integration.Salesforce.Testing.Library.ModelTests
         public void LocationToString_ReturnsValidString_True()
         {
             // assemble
-            Address location = md.Location();
+            Address location = md.Address();
             // act
             var response = location.ToString();
             var teststring = $"ADDRESS{{StreetAddress:{location.StreetAddress};City:{location.City};State:{location.State};Zip:{location.Zip};}}";
