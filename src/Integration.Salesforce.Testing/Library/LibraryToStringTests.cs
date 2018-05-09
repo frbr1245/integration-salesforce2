e64cccb3ffd51b1b393694ab1fc5275ef8db7d79
derek white <derekbwhite80@gmail.com>
2018-05-09 14:30:48 -0400

    remove email tostring test [#157436826]

--- src/Integration.Salesforce.Testing/Library/LibraryToStringTests.cs
+++ src/Integration.Salesforce.Testing/Library/LibraryToStringTests.cs
@@ -15,12 +15,24 @@ namespace Integration.Salesforce.Testing.Library.ModelTests
             Person trainer = md.Trainer();
             Address location = md.Location();
             Batch batch = md.Batch();
-            EMail email = md.EMail();
             // act
             var response = trainer.ToString();
             var teststring = $"PERSON{{Name:{trainer.FirstName} {trainer.LastName};Phone:{trainer.Phone};Role:{trainer.Role};HasCar:{trainer.HasCar};BatchName:{trainer.BatchName};}}";
             teststring += location.ToString();
             teststring += email.ToString();
+            // assert
+            Assert.NotNull(response);
+            Assert.Equal(teststring, response, true);
+        }
+
+        [Fact]
+        public void LocationToString_ReturnsValidString_True()
+        {
+            // assemble
+            Address location = md.Location();
+            // act
+            var response = location.ToString();
+            var teststring = $"ADDRESS{{StreetAddress:{location.StreetAddress};City:{location.City};State:{location.State};Zip:{location.Zip};}}";
             // assert
             Assert.NotNull(response);
             Assert.Equal(teststring, response, true);
